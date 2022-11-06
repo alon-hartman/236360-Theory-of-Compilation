be3ef7e3423189ef2dab18a13b73b2f94fc8da09
Alon Hartman <alon.hartman@campus.technion.ac.il>
2022-11-06 18:21:44 +0200

    [HW1] Removed unneccecary code

--- hw1/hw1.cpp
+++ hw1/hw1.cpp
@@ -1,35 +1,41 @@
-#include "tokens.hpp"
 #include <array>
 #include <cstring>
 #include <iostream>
 #include <sstream>
 
+#include "tokens.hpp"
+
+using namespace std;
+
 int main()
 {
     int token;
-    std::array<const char *, ENUM_SIZE> token_to_string = {
-        "",         "VOID",  "INT",     "BYTE",   "B",      "BOOL",   "AND",    "OR",
-        "NOT",      "TRUE",  "FALSE",   "RETURN", "IF",     "ELSE",   "WHILE",  "BREAK",
-        "CONTINUE", "SC",    "COMMA",   "LPAREN", "RPAREN", "LBRACE", "RBRACE", "ASSIGN",
-        "RELOP",    "BINOP", "COMMENT", "ID",     "NUM",    "STRING", "ERROR",  "UNRECOGNIZED_CHAR"};
+    // clang-format off
+    array<const char *, ENUM_SIZE> token_to_string = {"", "VOID", "INT", "BYTE", "B", "BOOL", "AND", "OR", "NOT",
+                                                      "TRUE", "FALSE", "RETURN", "IF", "ELSE", "WHILE", "BREAK",
+                                                      "CONTINUE", "SC", "COMMA", "LPAREN", "RPAREN", "LBRACE", "RBRACE",
+                                                      "ASSIGN", "RELOP", "BINOP", "COMMENT", "ID", "NUM", "STRING",
+                                                      "ERROR", "UNRECOGNIZED_CHAR", "UNCLOSED_STRING", "HEX_LENGTH_0",
+                                                      "HEX_LENGTH_1", "ESCAPE_ERROR"};
+    // clang-format on
     while ((token = yylex()))
     {
         switch (token)
         {
         case COMMENT:
-            std::cout << yylineno << " " << token_to_string[token] << " //" << std::endl;
+            cout << yylineno << " " << token_to_string[token] << " //" << endl;
             break;
         case STRING: {
-            std::stringstream buffer;
+            stringstream buffer;
             buffer << yylineno << " " << token_to_string[token] << " ";
-            std::string input_text(yytext);
+            string input_text(yytext);
             for (int i = 1; i < input_text.length() - 1; ++i)
             {
                 if (input_text[i] == '\\')
                 {
                     if (i == input_text.length() - 2)
                     {
-                        std::cout << "Error unclosed string" << std::endl;
+                        cout << "Error unclosed string" << endl;
                         exit(0);
                     }
                     i++;
@@ -46,12 +52,12 @@ int main()
                             if (!((('0' <= c1 && c1 <= '9') || ('A' <= c1 && c1 <= 'F')) &&
                                   (('0' <= c2 && c2 <= '9') || ('A' <= c2 && c2 <= 'F'))))
                             {
-                                throw std::invalid_argument("");
+                                throw invalid_argument("");
                             }
                         }
-                        catch (std::invalid_argument)
+                        catch (invalid_argument)
                         {
-                            std::cout << "Error undefined escape sequence " << input_text.substr(i, 3) << std::endl;
+                            cout << "Error undefined escape sequence " << input_text.substr(i, 3) << endl;
                             exit(0);
                         }
                         buffer << (char)std::stoi(input_text.substr(i + 1, 2), nullptr, 16);
@@ -61,7 +67,7 @@ int main()
                         buffer << '\t';
                         break;
                     case 'n':
-                        buffer << std::endl;
+                        buffer << endl;
                         break;
                     case '0':
                         buffer << '\0';
@@ -73,7 +79,7 @@ int main()
                         buffer << '"';
                         break;
                     default:
-                        std::cout << "Error undefined escape sequence " << input_text[i] << std::endl;
+                        cout << "Error undefined escape sequence " << input_text[i] << endl;
                         exit(0);
                     }
                 }
@@ -82,17 +88,17 @@ int main()
                     buffer << input_text[i];
                 }
             }
-            std::cout << buffer.str() << std::endl;
+            cout << buffer.str() << endl;
             break;
         }
         case UNRECOGNIZED_CHAR:
-            std::cout << "Error " << yytext[std::strlen(yytext) - 1] << std::endl;
+            cout << "Error " << yytext[strlen(yytext) - 1] << endl;
             exit(0);
         case UNCLOSED_STRING:
-            std::cout << "Error unclosed string" << std::endl;
+            cout << "Error unclosed string" << endl;
             exit(0);
         case ESCAPE_ERROR: {
-            std::string input_text = yytext;
+            string input_text = yytext;
             for (int i = 1; i < input_text.length() - 1; ++i)
             {
                 if (input_text[i] == '\\')
@@ -101,48 +107,24 @@ int main()
                     if (input_text[i] != 'n' || input_text[i] != 't' || input_text[i] != '0' || input_text[i] != 'r' ||
                         input_text[i] != '\\' || input_text[i] != '\"')
                     {
-                        std::cout << "Error undefined escape sequence " << input_text[i] << std::endl;
+                        cout << "Error undefined escape sequence " << input_text[i] << endl;
                         exit(0);
                     }
                 }
             }
             exit(0);
         }
-        case HEX_ERROR: {
-            std::string input_text = yytext;
-            for (int i = 1; i < input_text.length() - 1; ++i)
-            {
-                if (input_text[i] == '\\')
-                {
-                    i++;
-                    if (input_text[i] == 'x')
-                    {
-                        try
-                        {
-                            std::stoi(input_text.substr(i + 1, 1), nullptr, 16);
-                            std::stoi(input_text.substr(i + 2, 1), nullptr, 16);
-                        }
-                        catch (std::invalid_argument &)
-                        {
-                            std::cout << "Error undefined escape sequence " << input_text.substr(i, 3) << std::endl;
-                            exit(0);
-                        }
-                    }
-                }
-            }
-            exit(0);
-        }
         case HEX_LENGTH_0:
-            std::cout << "Error undefined escape sequence x" << std::endl;
+            cout << "Error undefined escape sequence x" << endl;
             exit(0);
         case HEX_LENGTH_1:
-            std::cout << "Error undefined escape sequence x" << yytext[strlen(yytext) - 2] << std::endl;
+            cout << "Error undefined escape sequence x" << yytext[strlen(yytext) - 2] << endl;
             exit(0);
         case ERROR:
-            std::cout << "Error " << yytext << std::endl;
+            cout << "Error " << yytext << endl;
             exit(0);
         default:
-            std::cout << yylineno << " " << token_to_string[token] << " " << yytext << std::endl;
+            cout << yylineno << " " << token_to_string[token] << " " << yytext << endl;
             break;
         }
     }
--- hw1/scanner.lex
+++ hw1/scanner.lex
@@ -15,40 +15,40 @@ whitespace  ([\t\n\r ])
 id          {letter}({letter}|{digit})*
 
 %%
-void								return VOID;
-int									return INT;
-byte								return BYTE;
-bool								return BOOL;
-and									return AND;
-or									return OR;
-not									return NOT;
-true								return TRUE;
-false								return FALSE;
-return								return RETURN;
-if									return IF;
-else								return ELSE;
-while								return WHILE;
-break								return BREAK;
-continue							return CONTINUE;
-b									return B;
-;									return SC;
-,									return COMMA;
-\(									return LPAREN;
-\)									return RPAREN;
-\{									return LBRACE;
-\}									return RBRACE;
-=									return ASSIGN;
-==|!=|<=|>=|<|>						return RELOP;
-[\+\-\*\/]							return BINOP;
-\/\/([^\n\r])+|\/\/			return COMMENT; /* TODO: check if need to catch empty comment ("//\n") */
-{id}								return ID;
-0[1-9][0-9]*						return ERROR; 
-([1-9]{digit}*)|(0)					return NUM;
-{digit}+\.{digit}+					return ERROR;  /* fail on float */
-\"([^\"])*[\x0A\x0D]+.*\"|\"([^\"\n\r])*		return UNCLOSED_STRING;  /* fail on unclosed string */
-(\"(.*)([^\\])((\\){2})*\\x.\")|(\"((\\){2})*\\x.\") 							return HEX_LENGTH_1;  /* fail on illegal hex character */
-(\".*[^\\]((\\){2})*\\x\")|(\"((\\){2})*\\x\") 							return HEX_LENGTH_0;  /* fail on illegal hex character */ /*\"([^\n\r])*([^\\]\")+([.]{-}[\n\r])*\"				 	printf("illegal \" error\n");return ERROR;   /* fail on " */
-(\"([^(\n)(\\)(\r)]*)([^\\])((\\){2})*\\[^nrxt0\\\"][^(\n)(\\)(\r)]*\")|(\"((\\){2})*\\[^nrxt0\\\"][^(\n)(\")(\\)(\r)]*\") return ESCAPE_ERROR;
-\"([\x00-\x09\x0b-\x0c\x0e-\x21\x23-\x5b\x5d-\x7f]|((\\)(\\))|((\\)(\"))|((\\)(n))|((\\)(r))|((\\)(t))|((\\)(0))|((\\)x))*[^\"\n]*\"			return STRING; /* 0x9=TAB */
+void													return VOID;
+int														return INT;
+byte													return BYTE;
+bool													return BOOL;
+and														return AND;
+or														return OR;
+not														return NOT;
+true													return TRUE;
+false													return FALSE;
+return													return RETURN;
+if														return IF;
+else													return ELSE;
+while													return WHILE;
+break													return BREAK;
+continue												return CONTINUE;
+b														return B;
+;														return SC;
+,														return COMMA;
+\(														return LPAREN;
+\)														return RPAREN;
+\{														return LBRACE;
+\}														return RBRACE;
+=														return ASSIGN;
+==|!=|<=|>=|<|>											return RELOP;
+[\+\-\*\/]												return BINOP;
+\/\/([^\n\r])+|\/\/										return COMMENT;
+{id}													return ID;
+0[1-9][0-9]*											return ERROR; /* fail on leading zero in number */
+{digit}+\.{digit}+										return ERROR;  /* fail on float */
+([1-9]{digit}*)|(0)										return NUM;
+\"([^\"])*[\x0A\x0D]+.*\"|\"([^\"\n\r])*				return UNCLOSED_STRING;  /* fail on unclosed string */
+(\"(.*)([^\\])((\\){2})*\\x.\")|(\"((\\){2})*\\x.\") 	return HEX_LENGTH_1;  /* fail on illegal hex character after \x */
+(\".*[^\\]((\\){2})*\\x\")|(\"((\\){2})*\\x\") 			return HEX_LENGTH_0;  /* fail on \x */
+(\"([^(\n)(\\)(\r)]*)([^\\])((\\){2})*\\[^nrxt0\\\"][^(\n)(\\)(\r)]*\")|(\"((\\){2})*\\[^nrxt0\\\"][^(\n)(\")(\\)(\r)]*\") 				return ESCAPE_ERROR;
+\"([\x00-\x09\x0b-\x0c\x0e-\x21\x23-\x5b\x5d-\x7f]|((\\)(\\))|((\\)(\"))|((\\)(n))|((\\)(r))|((\\)(t))|((\\)(0))|((\\)x))*[^\"\n]*\"	return STRING; /* 0x9=TAB */
 {whitespace}						{ /* ignore */ };
-.									return UNRECOGNIZED_CHAR;
+.														return UNRECOGNIZED_CHAR;
