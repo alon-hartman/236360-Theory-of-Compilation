a3d953a1b4f5012c59f581e8e94ea6716ca4789d
Alon Hartman <alon.hartman@campus.technion.ac.il>
2022-12-17 14:18:03 +0200

    [HW3] some more adjustments following some more tests

--- hw3/source.hpp
+++ hw3/source.hpp
@@ -16,9 +16,7 @@ enum class types
   Bool,
   Byte,
   String,
-  Func,
-  Void,
-  Operator
+  Void
 };
 
 const int BYTE_SIZE = 255;
@@ -29,52 +27,9 @@ struct Node
   virtual ~Node() {}
   types m_type;
   std::string m_name;
-  int m_num_val;
-  std::string m_string_val;
   int m_lineno;
 
   std::vector<types> m_types_list;
-
-  virtual void setValue(Node *other)
-  {
-    switch (other->m_type)
-    {
-    case types::Int:
-    case types::Bool:
-    case types::Byte:
-      m_num_val = other->m_num_val;
-      return;
-    case types::String:
-      m_string_val = other->m_string_val;
-      return;
-    default:
-      return;
-    }
-  }
-  virtual void setValue(Node *lhs, Node *op, Node *rhs)
-  {
-    assert(op->m_type == types::Operator);
-    if (op->m_name == "==")
-      this->m_num_val = (lhs->m_num_val == rhs->m_num_val);
-    else if (op->m_name == "!=")
-      this->m_num_val = (lhs->m_num_val != rhs->m_num_val);
-    else if (op->m_name == "<")
-      this->m_num_val = (lhs->m_num_val < rhs->m_num_val);
-    else if (op->m_name == ">")
-      this->m_num_val = (lhs->m_num_val > rhs->m_num_val);
-    else if (op->m_name == "<=")
-      this->m_num_val = (lhs->m_num_val <= rhs->m_num_val);
-    else if (op->m_name == ">=")
-      this->m_num_val = (lhs->m_num_val >= rhs->m_num_val);
-    else if (op->m_name == "+")
-      this->m_num_val = (lhs->m_num_val + rhs->m_num_val);
-    else if (op->m_name == "-")
-      this->m_num_val = (lhs->m_num_val - rhs->m_num_val);
-    else if (op->m_name == "*")
-      this->m_num_val = (lhs->m_num_val * rhs->m_num_val);
-    else if (op->m_name == "/")
-      this->m_num_val = (lhs->m_num_val / rhs->m_num_val);
-  }
 
   virtual void setName(Node *other)
   {
@@ -88,10 +43,6 @@ struct Node
   {
     this->m_type = other->m_type;
   }
-  void SetValue(std::string string_val)
-  {
-    m_string_val = string_val;
-  }
 
   bool isNumType()
   {
@@ -103,18 +54,10 @@ struct Num : public Node
 {
   Num(const char *yytext = "", int yylineno = -1) : Node(yytext, yylineno)
   {
-    m_num_val = std::stoi(yytext);
     m_type = types::Int;
   }
   ~Num() {}
 };
-
-struct OpNode : public Node
-{
-  OpNode(const char *yytext = "", int yylineno = -1) : Node(yytext, yylineno) { m_type = types::Operator; }
-  ~OpNode() {}
-};
-
 struct IntType : public Node
 {
   IntType(const char *yytext = "", int yylineno = -1) : Node(yytext, yylineno) { m_type = types::Int; }
@@ -144,7 +87,6 @@ struct StringType : public Node
   StringType(const char *yytext = "", int yylineno = -1) : Node(yytext, yylineno)
   {
     m_type = types::String;
-    m_string_val = yytext;
   }
   ~StringType() {}
 };
@@ -154,7 +96,6 @@ struct IDNode : public Node
   IDNode(const char *yytext = "", int yylineno = -1) : Node(yytext, yylineno)
   {
     m_type = types::None;
-    m_string_val = yytext;
   }
   ~IDNode() {}
 };
